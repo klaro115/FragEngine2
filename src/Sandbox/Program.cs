433f37276259e8a05f3eccaf7d465c2009d4f18e
Roby Desktop <klaro115@hotmail.com>
2025-07-28 00:00:22 +0200

    Added basic sandbox project for testing

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,29 @@
+﻿using FragEngine.EngineCore;
+using FragEngine.Helpers;
+
+Console.WriteLine("\n### BEGIN ###\n");
+
+Engine? engine = null;
+try
+{
+	Console.WriteLine("Creating engine...");
+
+	if (EngineStartupHelper.CreateDefaultEngine(out engine))
+	{
+		Console.WriteLine("Starting engine...");
+
+		engine!.Run();
+
+		Console.WriteLine("Engine has exited.");
+	}
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"FATAL: Unhandled exception caused engine crash!\nException: {ex}");
+}
+finally
+{
+	engine?.Dispose();
+}
+
+Console.WriteLine("\n#### END ####");
